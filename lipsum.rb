b1db14a321c4de9e2e210946f9e8665a7ac5139b
Marie Denney <mldenne@gmail.com>
2016-09-29 21:17:24 -0400

    Refactoring files

--- lipsum.rb
+++ lipsum.rb
@@ -1,6 +1,10 @@
+# !/usr/bin/env ruby
+
 require 'erb'
 
-lipsum_wanted = ARGV[0]
+# lipsum_wanted = ARGV[0]
+# lipsum_number = ARGV[1] || 1
+# lipsum_number = lipsum_number.to_i
 
 puts %{
   For your Ipsum, please select one of the following:
@@ -19,38 +23,23 @@ puts %{
 print ">> "
 
 lipsum_number = gets.chomp
-if lipsum_number = 0 || nil
-  lipsum_number = 1
-end
+# if lipsum_number = 0 || nil
+#   lipsum_number = 1
+# end
 lipsum_number = lipsum_number.to_i
 
 if lipsum_wanted == 1
-  lipsum_wanted = lipsum_wanted.to_s.downcase
-  lipsum_wanted = "old_fashioned"
-  title = "Old-Fashioned"
-  content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." * lipsum_number
+  html_title = "old_fashioned"
   name = "Old-Fashioned"
-  new_file = File.open("./#{lipsum_wanted}.html", "w+")
-  new_file << ERB.new(File.read("index.html.erb")).result(binding)
-  new_file.close
+  content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n\n"
 elsif lipsum_wanted == 2
-  lipsum_wanted = lipsum_wanted.to_s.downcase
-  lipsum_wanted = "cool_hipster"
-  title = "Cool Hipster"
-  content = "Whatever semiotics listicle put a bird on it biodiesel. Aesthetic authentic kombucha disrupt. Flannel kickstarter kale chips scenester chartreuse occupy. Flannel art party ethical, hella bespoke retro affogato mumblecore dreamcatcher banjo actually locavore iPhone brunch 8-bit. Kickstarter celiac meditation, narwhal XOXO microdosing wolf etsy four dollar toast gluten-free marfa pinterest cornhole. Asymmetrical cardigan synth celiac, echo park chicharrones biodiesel master cleanse drinking vinegar man braid deep v you probably haven't heard of them blog distillery. Green juice mustache poutine knausgaard roof party before they sold out normcore, flexitarian cliche tacos man braid scenester wayfarers plaid man bun." * lipsum_number
+  html_title = "cool_hipster"
   name = "Cool Hipster"
-  new_file = File.open("./#{lipsum_wanted}.html", "w+")
-  new_file << ERB.new(File.read("index.html.erb")).result(binding)
-  new_file.close
+  content = "Whatever semiotics listicle put a bird on it biodiesel. Aesthetic authentic kombucha disrupt. Flannel kickstarter kale chips scenester chartreuse occupy. Flannel art party ethical, hella bespoke retro affogato mumblecore dreamcatcher banjo actually locavore iPhone brunch 8-bit. Kickstarter celiac meditation, narwhal XOXO microdosing wolf etsy four dollar toast gluten-free marfa pinterest cornhole. Asymmetrical cardigan synth celiac, echo park chicharrones biodiesel master cleanse drinking vinegar man braid deep v you probably haven't heard of them blog distillery. Green juice mustache poutine knausgaard roof party before they sold out normcore, flexitarian cliche tacos man braid scenester wayfarers plaid man bun.\n\n"
 elsif lipsum_wanted == 3
-  lipsum_wanted = lipsum_wanted.to_s.downcase
-  lipsum_wanted = "dumb_ass"
-  title = "Dumb Ass"
-  content = "Normally, both your asses would be dead as fucking fried chicken, but you happen to pull this shit while I'm in a transitional period so I don't wanna kill you, I wanna help you. But I can't give you this case, it don't belong to me. Besides, I've already been through too much shit this morning over this case to hand it over to your dumb ass."
-  name = "Dumb Ass" * lipsum_number
-  new_file = File.open("./#{lipsum_wanted}.html", "w+")
-  new_file << ERB.new(File.read("index.html.erb")).result(binding)
-  new_file.close
+  html_title = "dumb_ass"
+  name = "Dumb Ass"
+  content = "\nNormally, both your asses would be dead as fucking fried chicken, but you happen to pull this shit while I'm in a transitional period so I don't wanna kill you, I wanna help you. But I can't give you this case, it don't belong to me. Besides, I've already been through too much shit this morning over this case to hand it over to your dumb ass.\n\n"
 else
   puts %{
     You have entered an invalid selection.
@@ -60,3 +49,8 @@ else
     3. For Dumb Ass Ipsum.
   }
 end
+new_file = File.open("./#{html_title}.html", "w+")
+new_file << ERB.new(File.read("index.html.erb")).result(binding)
+new_file.close
+
+# system("echo '#{{paragraph}}' | pbcopy")
